abad2ad90536c34d0377a9c9c1c1e3a6b135e8f9
Tobias Leich <email@froggs.de>
2014-05-02 11:34:53 +0200

    compile fault4.pm instead of writing to it

--- fault4.bat
+++ fault4.bat
@@ -1,4 +1,4 @@
-echo "class Foo; $?PACKAGE.^add_fallback( sub ( $object, $name ) { True }, sub ( $object, $name ) { } );" > fault4_dep.pm
-call perl6-m -I. --target=mbc --output=fault4_dep.moarvm fault4_dep.pm
-call perl6-m -I. --target=mbc --output=fault4.moarvm -Mfault4_dep -e1
-call perl6-m -I. -Mfault4 -e1
+REM echo "class Foo; $?PACKAGE.^add_fallback( sub ( $object, $name ) { True }, sub ( $object, $name ) { } );" > fault4_dep.pm
+call perl6-m -I. --target=mbc --output=fault4.moarvm fault4.pm
+call perl6-m -I. --target=mbc --output=fault41.moarvm -Mfault4 -e1
+call perl6-m -I. -Mfault41 -e1
--- fault4.sh
+++ fault4.sh
@@ -1,4 +1,4 @@
-echo 'class Foo; $?PACKAGE.^add_fallback( sub ( $object, $name ) { True }, sub ( $object, $name ) { } );' > fault4_dep.pm
-perl6-m -I. --target=mbc --output=fault4_dep.moarvm fault4_dep.pm
-perl6-m -I. --target=mbc --output=fault4.moarvm -Mfault4_dep -e1
-perl6-m -I. -Mfault4 -e1
+#echo 'class Foo; $?PACKAGE.^add_fallback( sub ( $object, $name ) { True }, sub ( $object, $name ) { } );' > fault4_dep.pm
+perl6-m -I. --target=mbc --output=fault4.moarvm fault4.pm
+perl6-m -I. --target=mbc --output=fault41.moarvm -Mfault4 -e1
+perl6-m -I. -Mfault41 -e1
